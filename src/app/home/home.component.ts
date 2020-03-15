28d2f69f8b89b579a8288c7de5969bcccacc28bf
sounishnath003 <flock.sinasini@gmail.com>
2020-03-15 22:49:53 +0530

    chartJS Init graph hooks

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../Services/home.service';
 import { FormGroup, FormControl } from '@angular/forms';
-import { Label, MultiDataSet } from 'ng2-charts';
+import { Label, MultiDataSet, SingleDataSet } from 'ng2-charts';
 import { ChartType } from 'chart.js';
 import { FormattedUpdate } from '../models/countryDetails';
 
@@ -11,32 +11,54 @@ import { FormattedUpdate } from '../models/countryDetails';
   styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-  private response:any ;
+  private response: any;
   private country: string;
 
-  public _data: FormattedUpdate ;
-  
+  public _data: FormattedUpdate;
 
-  confirmedValue:number = 0 ;
-  recoveredValue:number = 0 ;
-  deathsValue:number = 0 ;
+  confirmedValue: number = 0;
+  recoveredValue: number = 0;
+  deathsValue: number = 0;
+
+  
+  // charting
+  
+  public doughnutChartLabels: Label[] = [
+    "Confirmation Issues",
+    "Affected Issues",
+    "Panademic Issues"
+  ];
+  public doughnutChartData: SingleDataSet;
+  public doughnutChartType: ChartType = "doughnut";
+  
+  // end
 
   constructor(private _homeService: HomeService) {}
 
   ngOnInit() {
     this.showdata("india");
+    this.doughnutChartData = [
+      this.confirmedValue,
+      this.recoveredValue,
+      this.deathsValue
+    ];
   }
   showdata(country: string) {
     this._homeService.getData(country).subscribe(res => {
-       this.response = res;
-       // this.formattedUpdate.confirmed = this.data.confirmed.value;
-       this._data = this.response ;
+      this.response = res;
+      // this.formattedUpdate.confirmed = this.data.confirmed.value;
+      this._data = this.response;
 
-       this.confirmedValue = this._data.confirmed.value;
-       this.recoveredValue = this._data.recovered.value;
-       this.deathsValue = this._data.deaths.value;
-      console.log(this.confirmedValue,this.deathsValue,this.recoveredValue)
+      this.confirmedValue = this._data.confirmed.value;
+      this.recoveredValue = this._data.recovered.value;
+      this.deathsValue = this._data.deaths.value;
+      console.log(this.confirmedValue, this.deathsValue, this.recoveredValue);
 
+      this.doughnutChartData = [
+        this.confirmedValue,
+        this.recoveredValue,
+        this.deathsValue
+      ];
     });
   }
 
@@ -49,23 +71,4 @@ export class HomeComponent implements OnInit {
     this.inputCountry.reset();
     console.log();
   }
-
-  // charting
-
-  public doughnutChartLabels: Label[] = [
-    "Confirmation Issues",
-    "Affected Issues",
-    "Panademic Issues"
-  ];
-  public doughnutChartData: MultiDataSet = [
-    [ 
-      this.confirmedValue,
-      this.recoveredValue,
-      this.deathsValue
-     ]
-  ];
-  public doughnutChartType: ChartType = "doughnut";
-
-  // end
-
 }
