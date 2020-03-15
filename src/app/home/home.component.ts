4a4ae31a72bfc0145e73af1d707c8e0654edc6da
sounishnath003 <flock.sinasini@gmail.com>
2020-03-15 20:30:58 +0530

    adding realtime Data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../Services/home.service';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Label, MultiDataSet } from 'ng2-charts';
+import { ChartType } from 'chart.js';
 
 @Component({
   selector: "app-home",
@@ -9,13 +11,14 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
   data: {};
-  country:string;
+  country: string;
+
   constructor(private _homeService: HomeService) {}
 
   ngOnInit() {
-    this.showdata('india');
+    this.showdata("india");
   }
-  showdata(country:string) {
+  showdata(country: string) {
     this._homeService.getData(country).subscribe(res => {
       this.data = res;
       console.log(res);
@@ -23,13 +26,29 @@ export class HomeComponent implements OnInit {
   }
 
   inputCountry: FormGroup = new FormGroup({
-    country : new FormControl(null)
+    country: new FormControl(null)
   });
-  onSubmit()
-  {
+  onSubmit() {
     this.country = this.inputCountry.get("country").value;
     this.showdata(this.country);
     this.inputCountry.reset();
     console.log();
   }
+
+  // charting
+
+  public doughnutChartLabels: Label[] = [
+    "Confirmation Issues",
+    "Affected Issues",
+    "Panademic Issues"
+  ];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+    [50, 150, 120],
+    [250, 130, 70],
+  ];
+  public doughnutChartType: ChartType = "doughnut";
+
+  // end
+
 }
