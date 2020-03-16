f7e387234db35bde072b9ba7b56b78ee8e5bcd4c
sounishnath003 <flock.sinasini@gmail.com>
2020-03-16 22:29:29 +0530

    coronaMaps Updated ~ Patched

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapsComponent } from './home/maps/maps.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'map-live-coverage', 
+    component: MapsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
