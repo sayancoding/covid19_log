e4d420a1d6a793572f09fb7d20504631a722b941
sounishnath003 <flock.sinasini@gmail.com>
2020-03-16 23:54:39 +0530

    chaing Data Pipe ()

--- src/app/daily-details/daily-details.component.ts
+++ src/app/daily-details/daily-details.component.ts
@@ -95,21 +95,21 @@ export class DailyDetailsComponent implements OnInit {
     }
   };
   public barChartLabels: Label[] = [
-    "2006",
-    "2007",
-    "2008",
-    "2009",
-    "2010",
-    "2011",
-    "2012"
+    "04-Mar-2020",
+    "08-Mar-2020",
+    "11-Mar-2020",
+    "12-Mar-2020",
+    "14-Mar-2020",
+    "15-Mar-2020",
+    "16-Mar-2020"
   ];
   public barChartType: ChartType = "bar";
   public barChartLegend = true;
   // public barChartPlugins = [pluginDataLabels];
 
   public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: "Series A" },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: "Series B" }
+    { data: [65, 59, 80, 81, 56, 55, 40], label: "China" },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: "Italy" }
   ];
 
   // 2nd graph
@@ -127,20 +127,19 @@ export class DailyDetailsComponent implements OnInit {
   ];
 
   public radarChartData: ChartDataSets[] = [
-    { data: [65, 59, 90, 81, 56, 55, 40], label: "Series A" },
-    { data: [28, 48, 40, 19, 96, 27, 100], label: "Series B" }
+    { data: [65, 59, 90, 81, 56, 55, 40], label: "China" },
+    { data: [28, 48, 40, -19, 96, 27, 100], label: "Italy" }
   ];
   public radarChartType: ChartType = "radar";
 
   // third chart
   public polarAreaChartLabels: Label[] = [
-    "Download Sales",
-    "In-Store Sales",
-    "Mail Sales",
-    "Telesales",
-    "Corporate Sales"
+    "Confirmation Issues",
+    "Affected Issues",
+    "Deaths Issues",
+    "Trackings"
   ];
-  public polarAreaChartData: SingleDataSet = [300, 500, 100, 40, 120];
+  public polarAreaChartData: SingleDataSet = [300, 500, 100, 140, 120];
   public polarAreaLegend = true;
 
   public polarAreaChartType: ChartType = "polarArea";
